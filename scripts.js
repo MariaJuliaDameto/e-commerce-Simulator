09cc0a1b1e30a605f73da9eb260cec288605a537
Maria Julia Dameto <107319655+MariaJuliaDameto@users.noreply.github.com>
2024-04-25 14:50:05 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function addToCart(productId) {
+    // Aqui você pode implementar a lógica para adicionar o produto ao carrinho
+    console.log("Produto " + productId + " adicionado ao carrinho.");
+}
